3b5d28e94432d3150721553625fbd549fcea7b67
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-10-01 18:45:52 -0500

    contenedor titulos

---
+++ front-cmr-pastry/src/components/ContenedorTitulos.jsx
@@ -0,0 +1,9 @@
+function ContenedorTitulos({Titulo}){
+    return(
+        <section>
+            <h1 className = "DiseñoTitulo">{Titulo}</h1>
+        </section>
+
+    )
+}
+export default ContenedorTitulos;
