d0158b8fee9b35dc3287304429cda59b6d51a947
Juliana Sarmiento <ljulianasarmientoa@gmail.com>
2021-10-01 17:58:54 -0500

    Guardar proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import ContenedorTitulos from './components/ContenedorTitulos';
+import './styles/estilos.css';
+
+function App() {
+  return (
+    <div className="DiseñoTitulo">
+      
+    <body>
+        <section>
+            <ContenedorTitulos Titulo = "Modificar venta"/>
+        </section>
+    </body>
+    
+    </div>
+  );
+}
+
+export default App;
