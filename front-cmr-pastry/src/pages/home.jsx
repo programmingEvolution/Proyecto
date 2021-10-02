93a2b6f65e6a0f11404f90d162d62812fa10d55b
Daniela Castaneda Marin <danielamarin941@gmail.com>
2021-10-01 19:11:00 -0500

    agregar header y pantalla de home

--- front-cmr-pastry/src/pages/home.jsx
+++ front-cmr-pastry/src/pages/home.jsx
@@ -1,5 +1,37 @@
-function Home() {
-    return <div>Hola , soy el home</div>;
+import React from 'react'
+
+const home = () => {
+    return (
+        <div>
+            <section>
+                <h1 class="tituloHome">Admin Juli's Pastry</h1>
+                    <p class="introduccion">Aplicacion web de  administracion de ventas para una empresa en crecimiento, donde se logra tener control de la gestion de roles de usuarios, productos, clientes, ventas, informes mensuales. 
+                    Con diferentes herramientas para visuaizar el flujo de productos y manejo de bases de datos.   
+                    </p>
+                </section>
+
+                <section>
+                    <h2 class="herramientas">Herramientas</h2>
+                        <div>
+                        <ul class="contenedorTarjetasHome">
+                            <li class="tarjetas">Gestion de Usuarios
+                                <img class="fotosHome" src="./media/gestion-de-usuarios.jpeg" alt="foto muffin"/>
+                            </li>
+                            <li class="tarjetas">Gestion de ventas
+                                <img class="fotosHome" src="./media/tortaEmpacada.jpeg" alt="foto muffin"/>
+                            </li>
+                            <li class="tarjetas">Gestion de productos
+                                <img class="fotosHome" src="./media/tortaEspiralGrande.jpeg" alt="foto muffin"/>
+                            </li>
+                            <li class="tarjetas">Gestion de clientes
+                                <img class="fotosHome" src="./media/gestion-de-clientes.jpeg" alt="foto muffin"/>
+                            </li>
+                        </ul>
+                    </div>
+            </section>
+        
+        </div>
+    )
 }
 
-export default Home; 
+export default home
