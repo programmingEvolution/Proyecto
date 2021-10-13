a25cf874dc35e6e66aaf9f2e5cc8087b42b02fef
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-10-13 10:13:11 -0500

    Realizar filtrado de datos en tabla de productos desde input de busqueda

--- front-cmr-pastry/src/pages/Productos.jsx
+++ front-cmr-pastry/src/pages/Productos.jsx
@@ -3,44 +3,47 @@ import { Link } from "react-router-dom";
 import edit from "../media/edit.png";
 import ContenedorTitulos from "../components/ContenedorTitulos";
 import { obtenerProductos } from "../utils/api";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Productos = () => {
-
   const [productos, setProductos] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
+  const [productosFiltrados, setProductosFiltrados] = useState(productos);
 
-  obtenerProductos( (response) => {
-   
- setProductos(response.data)
+  obtenerProductos((response) => {
+    setProductos(response.data);
+  });
 
- });
-  
- console.log(productos[4])
+  useEffect(() => {
+    setProductosFiltrados(
+    productos.filter((elemento)=>{
+      return JSON.stringify(elemento).toLowerCase().includes(busqueda.toLowerCase())
+    }));
+  }, [busqueda, productos]);
+
+ 
+
+ 
 
   return (
-    
-  
-    
     <div>
-
-
-      
       <section>
         <ContenedorTitulos Titulo="Gestión de productos" />
       </section>
       <section>
-        
         <ul className="contenedorBotonesgrises">
-          <form>
+          
             <section>
               <label className="labelForm" htmlFor="idProducto">
                 Buscar Producto:
               </label>
               <input
+              value={busqueda}
                 className="inputForm"
                 id="idProducto"
                 placeholder="Ingrese el nombre del producto"
                 type="ID"
+                onChange={e=>setBusqueda(e.target.value)}
               ></input>
             </section>
             <section className="flex-grow justify-between">
@@ -48,7 +51,7 @@ const Productos = () => {
                 Buscar
               </button>
             </section>
-          </form>
+       
         </ul>
       </section>
 
@@ -61,31 +64,31 @@ const Productos = () => {
       <section>
         <table>
           <thead>
-          <tr>
-            <td className="tituloColumna">IDproductos</td>
-            <td className="tituloColumna">Proveedor</td>
-            <td className="tituloColumna">Nombre</td>
-            <td className="tituloColumna">Precio unidad</td>
-            <td className="tituloColumna">Disponible</td>
-            <td className="tituloColumna">Editar</td>
-          </tr>
+            <tr>
+              <td className="tituloColumna">IDproductos</td>
+              <td className="tituloColumna">Proveedor</td>
+              <td className="tituloColumna">Nombre</td>
+              <td className="tituloColumna">Precio unidad</td>
+              <td className="tituloColumna">Disponible</td>
+              <td className="tituloColumna">Editar</td>
+            </tr>
           </thead>
           <tbody>
-          {productos.map((producto) =>(
-  <tr key={producto._id}>
-            <td className="filaImpar">{producto._id}</td>
-            <td className="filaImpar">{producto.proveedor}</td>
-            <td className="filaImpar">{producto.nombreProducto}</td>
-            <td className="filaImpar">{producto.precioUnidad}</td>
-            <td className="filaImpar">{producto.inventario}</td>
-            <Link to="/modificarproducto">
-              <span>
-                <img class="icono" src={edit} alt="Editar" />
-              </span>
-            </Link>
-          </tr>
-          ))}</tbody>
-         
+            {productosFiltrados.map((producto) => (
+              <tr key={producto._id}>
+                <td className="filaImpar">{producto._id}</td>
+                <td className="filaImpar">{producto.proveedor}</td>
+                <td className="filaImpar">{producto.nombreProducto}</td>
+                <td className="filaImpar">{producto.precioUnidad}</td>
+                <td className="filaImpar">{producto.inventario}</td>
+                <Link to="/modificarproducto">
+                  <span>
+                    <img class="icono" src={edit} alt="Editar" />
+                  </span>
+                </Link>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </section>
     </div>
