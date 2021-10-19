6865203c7d09f13ec7e3b878237d3bb4be03fb57
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-10-19 17:17:24 -0500

    Agregar ventas

--- front-cmr-pastry/src/utils/api.js
+++ front-cmr-pastry/src/utils/api.js
@@ -63,3 +63,30 @@ export const editarUsuario = async (id, data, successCallback, errorCallback) =>
   };
   await axios.request(options).then(successCallback).catch(errorCallback);
 };
+
+
+export const crearVenta = async (data, successCallback, errorCallback) => {
+  const options = {
+    method: "POST",
+    url: "http://localhost:5000/ventas/",
+    headers: { "Content-Type": "application/json" },
+    data,
+  };
+  await axios.request(options).then(successCallback).catch(errorCallback);
+};
+
+export const obtenerVentas = async (successCallback, errorCallback) => {
+    const options = { method: 'GET', url: 'http://localhost:5000/ventas/' };
+    await axios.request(options).then(successCallback).catch(errorCallback);
+    
+  };
+
+
+  export const eliminarVenta = async (id, successCallback, errorCallback) => {
+    const options = {
+      method: 'DELETE',
+      url: `http://localhost:5000/ventas/${id}/`,
+      headers: { 'Content-Type': 'application/json' },
+    };
+    await axios.request(options).then(successCallback).catch(errorCallback);
+  };
