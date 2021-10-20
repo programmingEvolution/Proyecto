88f451be0ec02aee533faf8e723bb978f7803c34
Daniela Castaneda Marin <danielamarin941@gmail.com>
2021-10-19 23:09:57 -0500

    modificaciones en los estilos del header de la pagina de home

--- front-cmr-pastry/tailwind.config.js
+++ front-cmr-pastry/tailwind.config.js
@@ -4,12 +4,31 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      _boxShadow: {
+        sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+        DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+        md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+        lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+        xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+        '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+        inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+        none: 'none',
+      },
+      get boxShadow() {
+        return this._boxShadow;
+      },
+      set boxShadow(value) {
+        this._boxShadow = value;
+      },
     },
     
     extend: {},
   },
   variants: {
     extend: {},
+    boxShadow: ['active'],
+    animation: ['motion-safe'],
   },
   plugins: [],
 }
